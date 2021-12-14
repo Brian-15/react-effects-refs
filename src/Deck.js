baf5ac34a4959a02bb13bf51fd253e6ae8011e1a
Brian-15 <bcirieco@gmail.com>
2021-12-13 21:00:32 -0800

    button will toggle drawing sequence, making it draw a card every second until pressed again

--- src/Deck.js
+++ src/Deck.js
@@ -6,40 +6,51 @@ import Card from './Card.js';
 const BASE_URL = 'http://deckofcardsapi.com/api/deck/';
 
 const Deck = () => {
-  const [deckId, setDeckId] = useState("");
-  const [remaining, setRemaining] = useState(0);
-  const [card, setCard] = useState({});
+  const [deck, setDeck] = useState({});
+  const [card, setCard] = useState(undefined);
+  const [isNotDrawing, setisNotDrawing] = useState(true);
+  const [intervalId, setIntervalId] = useState(0);
 
   useEffect(function fetchDeckOnStart() {
     async function fetchDeck() {
       const { data } = await axios.get(`${BASE_URL}new/shuffle/?deck_count=1`);
-      setDeckId(data.deck_id);
-      setRemaining(data.remaining);
-    }
+      setDeck({deckId: data.deck_id, remaining: data.remaining});
+    };
     fetchDeck();
-  }, []);
+  }, [card]);
 
-  const draw = async () => {
-    if (remaining <= 1) {
-      window.alert('Error: no cards remaining!');
-      return;
+  const draw = () => {
+    setisNotDrawing(!isNotDrawing);
+    if (isNotDrawing) {
+      const id = setInterval(async () => {
+        if (deck.remaining <= 1) {
+          window.alert('Error: no cards left');
+          clearInterval(intervalId);
+          return;
+        }
+        const { data } = await axios.get(`${BASE_URL}${deck.deckId}/draw/?count=1`);
+        setCard(data.cards[0]);
+        setDeck({deckId: deck.deckId, remaining: data.remaining});
+      }, 1000);
+      setIntervalId(id);
     }
-    const { data } = await axios.get(`${BASE_URL}${deckId}/draw/?count=1`);
-    setCard(data.cards[0]);
-    setRemaining(data.remaining);
+    else {
+      clearInterval(intervalId);
+    }
   };
-
+  
   return (
     <>
       <div>
-        <button onClick={draw}>Draw</button>
+        <button onClick={draw}>{isNotDrawing? 'Start' : 'Stop'}</button>
       </div>
+      {card === undefined ? <p>Cards will be displayed here</p> :
       <Card
         src={card.image}
         name={`${card.value} of ${card.suit}`}
         key={uuid()}
         codeName={card.code}
-      />
+      />}
     </>
   );
 };
